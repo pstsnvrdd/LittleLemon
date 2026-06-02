a18e615250e23ffc59d032d930cd1d32e2dd2dae
Nastassia Smalenskaya <kemorgenic@gmail.com>
2026-06-02 13:42:37 +0300

    models
    created models for booking and models

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restaurant',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'littlelemon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
+        'USER' : 'admindjango',
+        'PASSWORD' : 'employee@123!',
     }
 }
 
--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+#Booking model
+class Booking(models.Model):
+    name = models.CharField(max_length=200)
+    booking_date = models.DateField()
+    no_of_guests = models.SmallIntegerField(default=1)
+
+    def __str__(self): 
+        return self.name
+
+
+#Menu model
+class Menu(models.Model):
+   name = models.CharField(max_length=200) 
+   price = models.DecimalField(max_digits=5, decimal_places=2) 
+   inventory = models.IntegerField() 
+
+   def __str__(self):
+      return self.name
